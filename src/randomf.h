5b045366dd5553d1c9a4cca39eae6f5453b83019
amartinsmg <andremindurimg@gmail.com>
2026-04-20 16:28:09 -0300

    feat(randomf): upgrade PRNG algorithm
    - Uses 64-bit state for improved randomness
    - Implements a new robust PRNG algorithm
    - Replaces `stdlib.h`

--- src/randomf.h
+++ src/randomf.h
@@ -1,15 +1,16 @@
 #ifndef RANDOM_F_H
 #define RANDOM_F_H
 
-#include <stdlib.h>
+#include <stdint.h>
 
-static inline float randomf(unsigned int *x) {
-  *x ^= *x >> 16;
-  *x *= 0x85ebca6b;
-  *x ^= *x >> 13;
-  *x *= 0xc2b2ae35;
-  *x ^= *x >> 16;
-  return (float)*x / (float)0xffffffffU;
+static inline float randomf(uint64_t *x) {
+  *x += 0x9e3779b97f4a7c15ULL;
+  uint64_t z = *x;
+  z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;
+  z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
+  z = z ^ (z >> 31);
+
+  return (float)(uint32_t)(z >> 32) / 4294967295.0f;
 }
 
 #endif /* RANDOM_F_H */
