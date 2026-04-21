02943404b4af22db2e1a4013afa52e9f1bd0efa4
amartinsmg <andremindurimg@gmail.com>
2026-04-21 11:38:40 -0300

    feat: Add benchmarking utility
    - Provides `run_bench` function
    - Includes `now` helper for timing
    - Defines `bench_fn` type

---
+++ src/run-bench.h
@@ -0,0 +1,38 @@
+#ifndef RUN_BENCH_H
+#define RUN_BENCH_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
+typedef double (*bench_fn)(int64_t);
+
+static inline double now() {
+  struct timespec t;
+  clock_gettime(CLOCK_MONOTONIC, &t);
+  return t.tv_sec + t.tv_nsec * 1e-9;
+}
+
+static void run_bench(const char *name, bench_fn f, int64_t N, uint32_t RUNS) {
+  printf("\n%s\n", name);
+
+  double total_runtime = 0;
+
+  double val = f(N);
+  printf("Pi estimated: %lf\n", val);
+
+  volatile double sink;
+
+  for (int i = 0; i < RUNS; i++) {
+    double start = now();
+    sink = f(N);
+    double end = now();
+    total_runtime += end - start;
+  }
+
+  double mean = total_runtime / RUNS;
+
+  printf("Time = %.3f s\n", mean);
+}
+
+#endif /* RUN_BENCH_H */
