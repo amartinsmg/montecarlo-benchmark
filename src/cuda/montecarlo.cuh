5aaa0542eea034a4155e38057febac3417694e5e
amartinsmg <andremindurimg@gmail.com>
2026-04-21 12:05:32 -0300

    refactor(cuda): remove kernel implementation and include montecarlo-kernel

--- src/cuda/montecarlo.cuh
+++ src/cuda/montecarlo.cuh
@@ -1,27 +1,7 @@
 #ifndef MONTECARLO_CUDA_CUH
 #define MONTECARLO_CUDA_CUH
 
-#include "randomf.h"
-#include <cuda_runtime.h>
-#include <stdint.h>
-
-__global__ static void kernel_montecarlo(int64_t *counts, int64_t N) {
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  int stride = gridDim.x * blockDim.x;
-
-  uint64_t seed = 123456789ULL ^ idx * 0x9e3779b97f4a7c15ULL;
-  int64_t local_count = 0;
-
-  float x, y;
-
-  for (int64_t i = 0; i < N; i += stride) {
-    x = randomf(&seed);
-    y = randomf(&seed);
-    local_count += x * x + y * y <= 1 ? 1 : 0;
-  }
-
-  counts[idx] = local_count;
-}
+#include "montecarlo-kernel.cuh"
 
 double static montecarlo_cuda(int64_t N) {
   int64_t total = 0;
